9ce7baa7183b4cd0c4448fda2647293318c3d4dc
Chuck <charles.yu4@gmail.com>
2023-08-23 13:47:18 -0400

    creating free trial for 2023

--- mvc/controllers/subscriptionController.js
+++ mvc/controllers/subscriptionController.js
@@ -135,7 +135,7 @@ const createSubscriptionCheckout = async (session) => {
 		createdAt: now,
 		expires:
 			now < new Date('December 29, 2023 00:00:00')
-				? new Date('December 31, 9999 00:00:00')
+				? new Date('January 1, 9999 00:00:00')
 				: now.setFullYear(9999),
 	});
 
